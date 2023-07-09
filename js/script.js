b8ba50534d4ae83728dd2422ca34fe09a1acdf10
Noob-developer-sobur <soburkhan210@gmail.com>
2023-07-09 12:21:46 +0600

    JQ Slider

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    $('.banner-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        arrows: true,
+        prevArrow: ".left",
+        nextArrow: ".right",
+
+      });
+})
